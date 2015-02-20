e9d72c8283377a536578923a0e90d8820e7334b3
oleg <voenniy@gmail.com>
2015-02-20 07:09:47 +0300

    Чистое Yii приложение

---
+++ protected/controllers/SiteController.php
@@ -0,0 +1,15 @@
+<?php
+
+class SiteController extends Controller
+{
+    /**
+	 * This is the default 'index' action that is invoked
+	 * when an action is not explicitly requested by users.
+	 */
+	public function actionIndex()
+	{
+
+		$this->render('index');
+	}
+
+}
